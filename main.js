cf068546ffa967d6c32357eebbce349b633ac2f2
luanamoraes260407 <105320286+luanamoraes260407@users.noreply.github.com>
2023-10-20 10:46:41 -0300

    .

---
+++ main.js
@@ -0,0 +1,23 @@
+//Declaração/criação da funç/ao tocaSomPum
+function tocaSomPum
+document.querySelector(#som tecla pom').play()
+}//chamada ou inovação da função TocaSom ao clicar no botão
+document.querySelector(tecla pom').onclick=tocaSom
+
+//Declaração/criação da funç/ao tocaSomPum
+function tocaSomPum
+document.querySelector(#som tecla pom').play()
+}//chamada ou inovação da função TocaSom ao clicar no botão
+document.querySelector(tecla pom').onclick=tocaSom
+
+//Declaração/criação da funç/ao tocaSomPum
+function tocaSomPum
+document.querySelector(#som tecla pom').play()
+}//chamada ou inovação da função TocaSom ao clicar no botão
+document.querySelector(tecla pom').onclick=tocaSom
+
+//Declaração/criação da funç/ao tocaSomPum
+function tocaSomPum
+document.querySelector(#som tecla pom').play()
+}//chamada ou inovação da função TocaSom ao clicar no botão
+document.querySelector(tecla pom').onclick=tocaSom
