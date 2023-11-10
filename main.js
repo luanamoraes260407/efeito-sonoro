629086a8417ff8f5041b1172154a9d236e83bbb7
luanamoraes260407 <105320286+luanamoraes260407@users.noreply.github.com>
2023-11-10 11:10:58 -0300

    .

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ function tocaSomXD (idElementoAudio) {
 }
 //criando a referências contantes listaDeTeclas e buscando a classe coletiva.tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
+//criação da repetição "whilr" e trocado pelo "for"
 fot (letcontadoe = 0;< listaDeTeclas.length; contador++)
-    const tecla = listaDeTeclas [contador]
-    const efeito = teclas.classList[1];
-    const idAudio = "#som_"${efeito};
-    teclas.onclick = function(){
-        tocaSom(idAudio);
+    const tecla = listaDeTeclas [contador] //guarda o contador da lita 
+    const efeito = teclas.classList[1];// chama 0 2° item clae individual do som 
+    const idAudio = "#som_"${efeito};//utiliza a junção template tring ${}
+    teclas.onclick = function(){// função anônima auxiliar para chamar tocaSom 
+        tocaSom(idAudio);// chmada da função tocaSom com prâmetro idAudio
     }
    
 }
