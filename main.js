0e5b8724e1fc78de808e1354a17338524da71569
luanamoraes260407 <105320286+luanamoraes260407@users.noreply.github.com>
2023-11-10 11:01:00 -0300

    .

--- main.js
+++ main.js
@@ -1,48 +1,16 @@
-//Declaração/criação da funç/ao tocaSomPum
-function tocaSomPum
-document.querySelector(#som tecla pom').play()
-}//chamada ou inovação da função TocaSom ao clicar no botão
-document.querySelector(.tecla pom').onclick=tocaSom
+//Declaração/criação de função tocaSomXD
+function tocaSomXD (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+//criando a referências contantes listaDeTeclas e buscando a classe coletiva.tecla
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//Declaração/criação da função tocaSomClap
-function tocaSomClap
-document.querySelector(#som_tecla_clap').play()
-}//chamada ou inovação da função TocaSom ao clicar no botão
-document.querySelector (.tecla clap').onclick=tocaSom
-
-//Declaração/criação da função tocaSomPumTim
-function tocaSomTim
-document.querySelector(#som_tecla_tim').play()
-}//chamada ou inovação da função TocaSom ao clicar no botão
-document.querySelector(.tecla tim').onclick=tocaTim
-
-//Declaração/criação da função tocaSomPuff
-function tocaSomPuff
-document.querySelector(#som_tecla_puf').play()
-}//chamada ou inovação da função TocaSom ao clicar no botão
-document.querySelector(.tecla Puff').onclick=tocaSom
-
-//Declaração/criação da função tocaSomSplach
-function tocaSomSplach
-document.querySelector(document.querySelector(').play()
-).play()
-}//chamada ou inovação da função TocaSom ao clicar no botão
-document.querySelector(.tecla Splach').onclick=tocaSom
-
-//Declaração/criação da função tocaSomToim
-function tocaSomToim
-document.querySelector(#som_tecla_clap').play()
-}//chamada ou inovação da função TocaSom ao clicar no botão
-document.querySelector(.tecla Toim').onclick=tocaSom
-
-//Declaração/criação da função tocaSomTic
-function tocaSomTic
-document.querySelector(#som_tecla_Tic').play()
-}//chamada ou inovação da função TocaSom ao clicar no botão
-document.querySelector(.tecla Tic').onclick=tocaSom
-
-//Declaração/criação da função tocaSomToc
-function tocaSomToc
-document.querySelector(#som_tecla_Toc').play()
-}//chamada ou inovação da função TocaSom ao clicar no botão
-document.querySelector(.tecla Toc').onclick=tocaSom
+fot (letcontadoe = 0;< listaDeTeclas.length; contador++)
+    const tecla = listaDeTeclas [contador]
+    const efeito = teclas.classList[1];
+    const idAudio = "#som_"${efeito};
+    teclas.onclick = function(){
+        tocaSom(idAudio);
+    }
+   
+}
